b73e923916aa1ff3325894eca1a7f78aca1222d4
LoveSKQ <loveskq@live.cn>
2020-02-06 09:54:32 +0800

    关阀指令修正为99H

--- water/src/main/java/com/learnbind/ai/iot/protocol/bean/MeterValveControlCmd.java
+++ water/src/main/java/com/learnbind/ai/iot/protocol/bean/MeterValveControlCmd.java
@@ -8,7 +8,7 @@ import com.learnbind.ai.iot.protocol.util.ByteUtil;
 public class MeterValveControlCmd extends MeterBase {
 
     public static final byte VALVE_OPEN             = (byte)0x55;   // 开
-    public static final byte VALVE_CLOSE            = (byte)0x59;   // 关
+    public static final byte VALVE_CLOSE            = (byte)0x99;   // 关
     public static final byte VALVE_OPEN_PER_50      = (byte)0xA1;   // 打开50%
     public static final byte VALVE_OPEN_PER_25      = (byte)0xA2;   // 打开25%
     public static final byte VALVE_OPEN_PER_12_5    = (byte)0xA3;   // 打开12.5%
